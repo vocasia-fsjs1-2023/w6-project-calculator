7838b7c81758675ed7360d77cfa59b50e349d3a0
nengmutiarahma <mutiarahma042@gmail.com>
2023-09-27 10:35:22 +0700

    calculator

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(value) {
+    const inputElement = document.getElementById('input');
+    inputElement.value += value;
+}
+
+function samaDengan() {
+    const inputElement = document.getElementById('input');
+    const resultElement = document.getElementById('result');
+    try {
+        const result = eval(inputElement.value);
+        if (!isNaN(result)) {
+            resultElement.textContent = `Hasil: ${result}`;
+        } else {
+            throw "Input tidak valid";
+        }
+    } catch (error) {
+        alert(error);
+    }
+}
+
+function clear() {
+    const inputElement = document.getElementById('input');
+    inputElement.value = '';
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = '';
+}
