c307bcc85e9e86e20111d34500de4893a9d05d5d
ainundea <ainundea29@gmail.com>
2023-10-02 18:26:39 +0700

    Inisialisasi proyek

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 let result = document.getElementById('screen');
 
 const display = (a) => {
-    if (a == 'clear'){
-        result.value = ''
-    } else if(a == 'del'){
-        result.value = result.value.slice(0,-1)
-    } else if(a == 'action'){
+    if (a == 'clear') {
+        result.value = '';
+    } else if (a == 'del') {
+        result.value = result.value.slice(0, -1);
+    } else if (a == 'action') {
         try {
-            result.value = eval(result.value)
+            // Periksa apakah ekspresi mengandung operator persen (%)
+            if (result.value.includes('%')) {
+                // Jika ada operator persen, hitung persentase
+                let expression = result.value.replace('%', '/100');
+                result.value = eval(expression);
+            } else {
+                // Jika tidak ada operator persen, hitung ekspresi biasa
+                result.value = eval(result.value);
+            }
         } catch (error) {
-            alert('error')
+            alert('Maaf kode yang Anda masukkan salah, Semangat!!!');
         }
     } else {
-        result.value += a
+        result.value += a;
     }
-    
-}
+};
